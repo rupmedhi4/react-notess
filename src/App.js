afd8a77872a9e80914a78bee1d358c2cbedc1c7e
Rup medhi <rupmmedhi@gmail.com>
2023-03-19 17:16:41 +0530

    Text-Util project with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import { useState } from 'react';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [text, setText] = useState("light mode");
+  const [alert, setAlert] = useState(null)
+
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+
+  }
+
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark")
+      setText("dark mode on")
+      document.body.style.backgroundColor = "#042743"
+      showAlert("Dark mode enable", "success")
+      document.title = "TextUtil - Dark Mode"
+    }
+    else {
+      setMode("light")
+      setText("light mode on")
+      document.body.style.backgroundColor = "white"
+      showAlert("Light mode enable", "success")
+      document.title = "TextUtil - Light Mode"
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <div>
+      {/* <Router> */}
+        <Navbar title="TextUtil" aboutText="About us" mode={mode} toggleMode={toggleMode} text={text} />
+        <Alert alert={alert} />
+
+        <div className="container my-3">
+          {/* <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
+          </Routes> */}
+          <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />
+        </div>
+
+      {/* </Router> */}
+      </div>
+    </>
+
+
+  )
 }
 
 export default App;
